d6baa783f8611fac60aa979942495c8882966019
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-03 05:59:18 -0400

    f   - integrated create inventory item command with post endpoint

--- src/main/java/control/tower/inventory/service/rest/InventoryItemsController.java
+++ src/main/java/control/tower/inventory/service/rest/InventoryItemsController.java
@@ -1,6 +1,8 @@
 package control.tower.inventory.service.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import control.tower.inventory.service.command.CreateInventoryItemCommand;
+import lombok.AllArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +12,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/inventory")
+@AllArgsConstructor
 public class InventoryItemsController {
 
-    @Autowired
     private Environment environment;
+    private CommandGateway commandGateway;
 
     @PostMapping
     public String createInventoryItem(@RequestBody CreateInventoryItemRestModel createInventoryItemRestModel) {
-        return "HTTP POST Handled " + createInventoryItemRestModel.getProductId();
+        CreateInventoryItemCommand createInventoryItemCommand = CreateInventoryItemCommand.builder()
+                .sku(UUID.randomUUID().toString())
+                .productId(createInventoryItemRestModel.getProductId())
+                .locationId(createInventoryItemRestModel.getLocationId())
+                .binId(createInventoryItemRestModel.getBinId())
+                .build();
+
+        String returnValue;
+
+        try {
+            returnValue = commandGateway.sendAndWait(createInventoryItemCommand);
+        } catch (Exception exception) {
+            returnValue = exception.getLocalizedMessage();
+        }
+
+        return returnValue;
     }
 
     @GetMapping
