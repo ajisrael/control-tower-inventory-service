b8c3336cf428d09b3c0a3ef8f5aed5e9fb2c8a00
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-03 21:18:19 -0400

    r   - replaced InventoryItemEventsHandler constructor with @AllArgsConstructor annotation

--- src/main/java/control/tower/inventory/service/query/InventoryItemEventsHandler.java
+++ src/main/java/control/tower/inventory/service/query/InventoryItemEventsHandler.java
@@ -3,18 +3,16 @@ package control.tower.inventory.service.query;
 import control.tower.inventory.service.core.data.InventoryItemEntity;
 import control.tower.inventory.service.core.data.InventoryItemRepository;
 import control.tower.inventory.service.core.events.InventoryItemCreatedEvent;
+import lombok.AllArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class InventoryItemEventsHandler {
 
     private final InventoryItemRepository inventoryItemRepository;
-
-    public InventoryItemEventsHandler(InventoryItemRepository inventoryItemRepository) {
-        this.inventoryItemRepository = inventoryItemRepository;
-    }
 
     @EventHandler
     public void on(InventoryItemCreatedEvent event) {
