622f714747fe3b0f24f535106dee073526b0094e
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-04 05:51:40 -0400

    f - added exception handlers to InventoryItemEventsHandler

--- src/main/java/control/tower/inventory/service/query/InventoryItemEventsHandler.java
+++ src/main/java/control/tower/inventory/service/query/InventoryItemEventsHandler.java
@@ -3,18 +3,35 @@ package control.tower.inventory.service.query;
 import control.tower.inventory.service.core.data.InventoryItemEntity;
 import control.tower.inventory.service.core.data.InventoryItemRepository;
 import control.tower.inventory.service.core.events.InventoryItemCreatedEvent;
-import lombok.AllArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 @ProcessingGroup("inventory-item-group")
 public class InventoryItemEventsHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(InventoryItemEventsHandler.class);
+
     private final InventoryItemRepository inventoryItemRepository;
+
+    public InventoryItemEventsHandler(InventoryItemRepository inventoryItemRepository) {
+        this.inventoryItemRepository = inventoryItemRepository;
+    }
+
+    @ExceptionHandler(resultType = Exception.class)
+    public void handle(Exception exception) throws Exception {
+        throw exception;
+    }
+
+    @ExceptionHandler(resultType = IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+        LOGGER.error(exception.getLocalizedMessage());
+    }
 
     @EventHandler
     public void on(InventoryItemCreatedEvent event) {
