f4f04d552a1d1492559651fded049987ad644d29
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-16 19:56:15 -0400

    f   - updated InventoryItemLookupEventsHandler to handle removed event

--- src/main/java/control/tower/inventory/service/command/InventoryItemLookupEventsHandler.java
+++ src/main/java/control/tower/inventory/service/command/InventoryItemLookupEventsHandler.java
@@ -3,10 +3,14 @@ package control.tower.inventory.service.command;
 import control.tower.inventory.service.core.data.InventoryItemLookupEntity;
 import control.tower.inventory.service.core.data.InventoryItemLookupRepository;
 import control.tower.inventory.service.core.events.InventoryItemCreatedEvent;
+import control.tower.inventory.service.core.events.InventoryItemRemovedEvent;
 import lombok.AllArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
+
+import static control.tower.core.utils.Helper.throwExceptionIfEntityDoesNotExist;
+import static control.tower.inventory.service.core.constants.ExceptionMessages.INVENTORY_ITEM_LOOKUP_ENTITY_WITH_ID_DOES_NOT_EXIST;
 
 @Component
 @AllArgsConstructor
@@ -18,5 +22,12 @@ public class InventoryItemLookupEventsHandler {
     @EventHandler
     public void on(InventoryItemCreatedEvent event) {
         inventoryItemLookupRepository.save(new InventoryItemLookupEntity(event.getSku()));
+    }
+
+    @EventHandler
+    public void on(InventoryItemRemovedEvent event) {
+        InventoryItemLookupEntity inventoryItemLookupEntity = inventoryItemLookupRepository.findBySku(event.getSku());
+        throwExceptionIfEntityDoesNotExist(inventoryItemLookupEntity, String.format(INVENTORY_ITEM_LOOKUP_ENTITY_WITH_ID_DOES_NOT_EXIST, event.getSku()));
+        inventoryItemLookupRepository.delete(inventoryItemLookupEntity);
     }
 }
